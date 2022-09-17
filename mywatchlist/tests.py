158a8f14158f493a6d7e082622a8a661d95d1abb
edutjie <edutjitrahardja@gmail.com>
2022-09-17 20:20:38 +0700

    Added response testing

--- mywatchlist/tests.py
+++ mywatchlist/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from django.test import Client
 
 # Create your tests here.
+class MyWatchListResponseTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_url_json_exists(self):
+        response = self.client.get("/mywatchlist/json/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_url_xml_exists(self):
+        response = self.client.get("/mywatchlist/xml/")
+        self.assertEqual(response.status_code, 200)
